144d67fef4838801dcc5b89dab5592d9b861fbf7
Aaron Toderash <adt150@mail.usask.ca>
2019-05-13 16:24:08 -0600

    moved CMakeLists.txt to build dir to cleanup build process

--- benchmark_code/runBenchmark.sh
+++ benchmark_code/runBenchmark.sh
@@ -6,6 +6,7 @@
 # 	-d [maximum degree] : Specifies the maximum degree for the aggregation phase.
 # 	--filter-singletons [0/1] : Specifies if single-node communities must be filtered (default: 0).
 # 	--leaf-aggregation [0/1] : Specifies if a pass of leaf aggregation must be applied (default: 0).
+#   NOTE: 0 means off.
 
 # run all datasets
 # VARS=(*)
