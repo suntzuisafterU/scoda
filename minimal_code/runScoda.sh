35f4fb2c8bc3935862c1489d9dcccbfda3efe664
Aaron Toderash <adt150@mail.usask.ca>
2019-05-16 15:47:35 -0600

    script

--- minimal_code/runScoda.sh
+++ minimal_code/runScoda.sh
@@ -1,23 +1,47 @@
 #!/bin/bash
 
 SCRIPT_NAME="$0"
-USAGE="Usage: \n $SCRIPT_NAME -f <in file path> -o <out file path> [positional args (if any)]"
+USAGE="Usage: \n $SCRIPT_NAME -f <in file path> -o <out file path> -d <degree threshold> -n <max node id> -m <number of edges> -i <number of lines to ignore>" 
 
-case $op in
-    -f )
-        INPATH="$1"
-        shift # past -f
-        shift # past value
-        ;;
-    -o )
-        OUTPATH="$1"
-        shift
-        shift
-        ;;
-esac
+while "$#" -ne 0; do
+    case "$1" in
+        -f)
+            INPATH="$1"
+            shift # past -f
+            shift # past value
+            ;;
+        -o)
+            OUTPATH="$1"
+            shift
+            shift
+            ;;
+        -d )
+            DEGREE="$1"
+            shift
+            shift
+            ;;
+        -n )
+            NUM_NODES="$1"
+            shift
+            shift
+            ;;
+        -m )
+            NUM_EDGES="$1"
+            shift
+            shift
+            ;;
+        -i )
+            IGNORE_LINES="$1"
+            shift
+            shift
+            ;;
+    esac
+done
 
 # Validate command line args
 if [ "$INPATH" = "" ] || [ "$OUTPATH" = "" ]; then
+    # TEmp testing
+    echo "in $INPATH, out: $OUTPATH"
     echo -e "$USAGE"
     exit 1
 fi
@@ -25,3 +49,5 @@ fi
 # source for next 2 lines: https://stackoverflow.com/questions/965053/extract-filename-and-extension-in-bash
 BASENAME=$(basename -- "$INPATH") # Extract base file name here.
 EXT="${BASENAME##*.}"# Extract extension. 
+
+./scoda $NUM_NODES $NUM_EDGES $DEGREE $IGNORE_LINES < "$INPATH" > "$OUTPATH"
