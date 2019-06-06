d0893c8c1e450923c4fb1a1182df3b380176bd4f
Aaron Toderash (adt150) <aaron.t@usask.ca>
2019-06-06 00:27:57 -0600

    init script downloads one dataset file

--- initData.sh
+++ initData.sh
@@ -2,5 +2,7 @@ if [ ! -d 'datasets' ]; then
   mkdir datasets
 fi
 
+curl -o datasets/com-amazon.ungraph.txt.gz https://snap.stanford.edu/data/bigdata/communities/com-amazon.ungraph.txt.gz
+gunzip datasets/com-amazon.ungraph.txt.gz
 # Then run the curl command in a loop with gunzip.  
 # May try to do this with xargs to use parallel processes
